563dcd794c75b669089965d8493b0211e25b4f6e
yewei <yewei@nibirutech.com>
2020-06-19 14:11:44 +0800

    add initial callback

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, Dispatch } from 'react'
 import produce, { Draft, castImmutable, Immutable } from 'immer'
 
-// type Reducer<U extends any[]> = (...args: U) => void
 type Tail<T extends any[]> = ((...t: T) => any) extends (
 	_: any,
 	...tail: infer TT
@@ -18,39 +17,49 @@ type Reducer<U extends any[]> = ((...args: U) => void) extends (
 
 class Store<U extends any[]> {
   state: Immutable<U[0]>
-  readonly listeners: Set<React.Dispatch<Immutable<U[0]>>>
+  readonly listeners: Set<Dispatch<Immutable<U[0]>>>
   readonly immerReducer: <Base extends Immutable<U[0]>>(base: Base, ...rest: Tail<Parameters<Reducer<U>>>) => Base
 
-  constructor(reducer: Reducer<U>, initialValue?: U[0]) {
+  constructor(reducer: Reducer<U>, initialValue: U[0]) {
     this.immerReducer = produce(reducer)
-    this.state = castImmutable(produce((initialValue ?? {}) as U[0], _ => {}))
+    this.state = castImmutable(produce(initialValue, _ => {}))
     this.listeners = new Set()
   }
 
-  dispatch(...args: Tail<Parameters<Reducer<U>>>) {
-    Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
+  async dispatch(...args: Tail<Parameters<Reducer<U>>>) {
+    return Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
       this.state = result
       this.listeners.forEach(listener => listener(result))
     })
   }
 }
 
-const immex = <U extends any[]>(reducer: Reducer<U>, initialValue?: U[0]) => {
+// type Immex<U extends any[]> = (reducer: Reducer<U>, initialValue?: U[0]) => () => [Immutable<U[0]>, (...args: Tail<Parameters<Reducer<U>>>) => void]
+type ImmexDispatcher<U extends any[]> = (...args: Tail<Parameters<Reducer<U>>>) => Promise<void>
+type ImmexCallBack<U extends any[]> = (dispatch: ImmexDispatcher<U>) => (() => any) | void
+
+const immex = <U extends any[]>(reducer: Reducer<U>, initialValue: U[0], callback?: ImmexCallBack<U>) => {
   const store = new Store<U>(reducer, initialValue)
-  return (): [Immutable<U[0]>, (...args: Tail<Parameters<Reducer<U>>>) => void] => {
+  return (): [Immutable<U[0]>, ImmexDispatcher<U>] => {
     const [localState, localUpdate] = useState(() => store.state)
-    const disptach: (...args: Tail<Parameters<Reducer<U>>>) => void = useCallback((...args) => {
+    const disptach: ImmexDispatcher<U> = useCallback((...args) => {
       if (!store.listeners.has(localUpdate)) {
         throw new Error('update is invalid before component rendered or after component destroyed.')
       }
-      store.dispatch(...args)
+      return store.dispatch(...args)
     }, [localUpdate])
     useEffect(() => {
       store.listeners.add(localUpdate)
+      const cleaner = (callback && store.listeners.size === 1)
+        ? callback(disptach)
+        : undefined
       return () => {
         store.listeners.delete(localUpdate)
+        if (cleaner && store.listeners.size === 0) {
+          cleaner()
+        }
       }
-    }, [])
+    }, [disptach])
     return [localState, disptach]
   }
 }
