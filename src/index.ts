45771ce2ce204f8b34881e532269d150eac237f3
yewei <yewei@nibirutech.com>
2020-08-10 16:34:48 +0800

    add dispatch task queue to ensure order

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { useState, useEffect, useCallback, Dispatch } from 'react'
 import produce, { Draft, castImmutable, Immutable } from 'immer'
 
@@ -15,26 +17,53 @@ type Reducer<U extends any[]> = ((...args: U) => void) extends (
   ? (...args: U) => void
   : never
 
+type Unlock = () => void
+
+const withLock = (() => {
+  const tasks: Unlock[] = []
+  let done = true
+  const sched = () => {
+    if (done && tasks.length > 0) {
+      done = false;
+      (tasks.shift() as Unlock)()
+    }
+  }
+  return (callback: () => Promise<void>) => new Promise<Unlock>((reslove) => {
+    const task = () => reslove(() => {
+      done = true
+      sched()
+    })
+    tasks.push(task);
+    (process && process.nextTick)
+      ? process.nextTick(sched)
+      : setImmediate(sched)
+  }).then(release => callback()
+    .then(() => {
+      release()
+    })
+    .catch(err => {
+      release()
+      throw err
+    })
+  )
+})()
+
 class Store<U extends any[]> {
   state: Immutable<U[0]>
   readonly listeners: Set<Dispatch<Immutable<U[0]>>>
   readonly immerReducer: <Base extends Immutable<U[0]>>(base: Base, ...rest: Tail<Parameters<Reducer<U>>>) => Base
-  lock: Promise<void>
 
   constructor(reducer: Reducer<U>, initialValue: U[0]) {
     this.immerReducer = produce(reducer)
     this.state = castImmutable(produce(initialValue, _ => {}))
     this.listeners = new Set()
-    this.lock = Promise.resolve()
   }
 
   async dispatch(...args: Tail<Parameters<Reducer<U>>>) {
-    await this.lock
-    this.lock = Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
+    return withLock(() => Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
       this.state = result
       this.listeners.forEach(listener => listener(result))
-    })
-    return this.lock
+    }))
   }
 }
 
