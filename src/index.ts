1dbbf98edb961ac159ab63511fea19d4f87e1c78
yewei <yewei@nibirutech.com>
2021-01-16 15:38:09 +0800

    add loading

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,7 @@ const locker = (): Locker => {
 class Store<U extends any[]> {
   state: Immutable<U[0]>
   readonly listeners: Set<Dispatch<Immutable<U[0]>>>
+  readonly loadings: Set<Dispatch<boolean>>
   readonly immerReducer: <Base extends Immutable<U[0]>>(base: Base, ...rest: Tail<Parameters<Reducer<U>>>) => Base
   readonly withLock: Locker
 
@@ -58,13 +59,18 @@ class Store<U extends any[]> {
     this.immerReducer = produce(reducer)
     this.state = castImmutable(produce(initialValue, _ => {}))
     this.listeners = new Set()
+    this.loadings = new Set()
   }
 
   async dispatch(...args: Tail<Parameters<Reducer<U>>>) {
-    return this.withLock(() => Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
-      this.state = result
-      this.listeners.forEach(listener => listener(result))
-    }))
+    return this.withLock(() => {
+      this.loadings.forEach(loading => loading(true))
+      return Promise.resolve(this.immerReducer(this.state, ...args)).then(result => {
+        this.state = result
+        this.listeners.forEach(listener => listener(result))
+        this.loadings.forEach(loading => loading(false))
+      })
+    })
   }
 }
 
@@ -73,16 +79,19 @@ type ImmexDispatcher<U extends any[]> = (...args: Tail<Parameters<Reducer<U>>>) 
 
 const immex = <U extends any[]>(reducer: Reducer<U>, initialValue: U[0]) => {
   const store = new Store<U>(reducer, initialValue)
-  return (): [Draft<Immutable<U[0]>>, ImmexDispatcher<U>] => {
+  return (): [Draft<Immutable<U[0]>>, ImmexDispatcher<U>, boolean] => {
     const [localState, localUpdate] = useState(() => store.state)
+    const [loading, loadingUpdate] = useState(false)
     const disptach: ImmexDispatcher<U> = useCallback((...args) => store.dispatch(...args), [])
     useEffect(() => {
+      store.loadings.add(loadingUpdate)
       store.listeners.add(localUpdate)
       return () => {
         store.listeners.delete(localUpdate)
+        store.loadings.delete(loadingUpdate)
       }
     }, [disptach])
-    return [castDraft(localState), disptach]
+    return [castDraft(localState), disptach, loading]
   }
 }
 
